ad11b5bf5ee04acba434ae6681053628f0c8d243
Chong Han Chua <johncch@gmail.com>
2025-10-18 10:48:44 -0700

    Update AI libraries

--- examples/scripts/simple-generate.ts
+++ examples/scripts/simple-generate.ts
@@ -1,5 +1,6 @@
 import { config } from "dotenv";
 import { z } from "zod";
+import { AIProvider, GenerationResult } from "../../src/ai/types.js";
 import { generate } from "../../src/index.js";
 import { getAllAxles } from "./helper.js";
 config();
@@ -26,12 +27,38 @@ for (const axle of axles) {
     tools: [callNameTool],
   });
 
-  console.log(`[RUN] ${axle.provider.name}, ${axle.provider.model}`);
-  console.log(`Result: ${result.type}`);
+  printResults(axle.provider, result);
+}
+
+function printResults(provider: AIProvider, result: GenerationResult) {
+  console.log(`\n[RUN] ${provider.name} (${provider.model})`);
+  console.log("=".repeat(50));
+  console.log(`${spacer("Result")}: ${result.type}`);
   if (result.type === "success") {
-    console.log(`Text: ${result.text}`);
+    console.log(`${spacer("Text")}: ${result.text}`);
+
+    if (result.toolCalls) {
+      for (const toolCall of result.toolCalls) {
+        console.log(`${spacer("ToolCall")}: ${toolCall.id}`);
+        console.log(`${spacer("Name", { indent: 2 })}: ${toolCall.name}`);
+        console.log(`${spacer("Arguments", { indent: 2 })}: ${stringify(toolCall.arguments)}`);
+      }
+    }
   } else {
     console.log(result.error.message);
   }
-  console.log("");
+}
+
+function stringify(input: any) {
+  if (typeof input === "string") {
+    return input;
+  }
+  return JSON.stringify(input);
+}
+
+function spacer(text: string, options: { length?: number; indent?: number } = {}) {
+  const { length = 12, indent = 0 } = options;
+  const indentation = " ".repeat(indent);
+  const paddedText = text.padEnd(length - indent, " ");
+  return indentation + paddedText;
 }
